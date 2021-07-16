47f5f78c021bc9a2f708b91851d9aeef48961c08
Erez Cohen <cohen.erez7@gmail.com>
2021-07-16 14:13:08 +0300

    degbugging

--- MergeSIDISandBANDSkimmers.C
+++ MergeSIDISandBANDSkimmers.C
@@ -133,14 +133,14 @@ void    Hello (int NeventsToMerge=10, int fdebug=2, int PrintProgress=5000){
         << std::endl;
     }
     std::cout << "You're all I've ever wanted, and your arms are open wide..." << std::endl;
-//    Int_t Nevents2Merge = CreateListOfEventsToMerge(BANDTree,
-//                                                    SIDISTree,
-//                                                    EventIDsToMerge,
-//                                                    BANDEventIndicesToMerge,
-//                                                    SIDISEventIndicesToMerge,
-//                                                    NeventsToMerge,
-//                                                    fdebug);
-//    std::cout << "hello-4..." << std::endl;
+    Int_t Nevents2Merge = CreateListOfEventsToMerge(BANDTree,
+                                                    SIDISTree,
+                                                    EventIDsToMerge,
+                                                    BANDEventIndicesToMerge,
+                                                    SIDISEventIndicesToMerge,
+                                                    NeventsToMerge,
+                                                    fdebug);
+    std::cout << "Cause you know just what to say, and you know just what to do..." << std::endl;
 //    if (fdebug>2) {
 //        std::cout << "Merging events (BAND/SIDIS):"  << std::endl;
 //        for (int i=0; i<Nevents2Merge; i++) {
