a8b0a29c8b8f457034578d043cfdd071c8cfb9d6
Erez Cohen <cohen.erez7@gmail.com>
2021-07-29 12:30:07 +0300

    debugging

--- Auxiliary/csv_reader.h
+++ Auxiliary/csv_reader.h
@@ -10,7 +10,7 @@
 
 class csv_reader {
     
-     
+    
     
     
     // based on [https://www.gormanalysis.com/blog/reading-and-writing-csv-files-with-cpp/]
@@ -23,18 +23,18 @@ class csv_reader {
     //
     //        return 0;
     //    }
-
+    
 public:
     csv_reader();
     ~csv_reader();
-   
+    
     
     std::vector<std::pair<std::string, double>> read_csv(std::string filename){
         // Reads a CSV file into a vector of <string, vector<int>> pairs where
         // each pair represents <column name, column values>
         
         // Create a vector of <string, int vector> pairs to store the result
-        std::vector<std::pair<std::string, std::vector<double>>> result;
+        std::vector<std::pair<std::string, double>> result;
         
         // Create an input filestream
         std::ifstream myFile(filename);
@@ -47,21 +47,21 @@ public:
         int val;
         
         // Read the column names
-//        if(myFile.good())
-//        {
-//            // Extract the first line in the file
-//            std::getline(myFile, line);
-//
-//            // Create a stringstream from line
-//            std::stringstream ss(line);
-//
-//            // Extract each column name
-//            while(std::getline(ss, colname, ',')){
-//
-//                // Initialize and add <colname, int vector> pairs to result
-//                result.push_back({colname, std::vector<double> {}});
-//            }
-//        }
+        //        if(myFile.good())
+        //        {
+        //            // Extract the first line in the file
+        //            std::getline(myFile, line);
+        //
+        //            // Create a stringstream from line
+        //            std::stringstream ss(line);
+        //
+        //            // Extract each column name
+        //            while(std::getline(ss, colname, ',')){
+        //
+        //                // Initialize and add <colname, int vector> pairs to result
+        //                result.push_back({colname, std::vector<double> {}});
+        //            }
+        //        }
         
         // Read data, line by line
         while(std::getline(myFile, line))
@@ -69,9 +69,9 @@ public:
             // Create a stringstream of the current line
             std::stringstream ss(line);
             
-//            // Keep track of the current column index
-//            int colIdx = 0;
-//
+            //            // Keep track of the current column index
+            //            int colIdx = 0;
+            //
             // Extract each value
             while(ss >> val){
                 
@@ -80,13 +80,13 @@ public:
                 
                 // If the next token is a comma, ignore it and move on
                 if(ss.peek() == ',') ss.ignore();
-
+                
                 // Add the current integer to the 'colIdx' column's values vector
                 result.second = val;
                 
-//                
-//                // Increment the column index
-//                colIdx++;
+                //
+                //                // Increment the column index
+                //                colIdx++;
             }
         }
         
