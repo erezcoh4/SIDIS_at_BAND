e96f03770eee6d336c871f08325f72e0ed354ce1
Erez Cohen <cohen.erez7@gmail.com>
2021-08-11 12:36:45 +0300

    debug

--- SIDISc12rSkimmer.C
+++ SIDISc12rSkimmer.C
@@ -56,7 +56,7 @@ bool EventPassedPionSelectionCutsCriteria (Double_t DC_x[3], Double_t DC_y[3],
                                            TVector3 Ve,      TVector3 Vpi );
 Double_t          Chi2PID_pion_lowerBound (Double_t p, Double_t C=0.88); // C(pi+)=0.88, C(pi-)=0.93
 Double_t          Chi2PID_pion_upperBound (Double_t p, Double_t C=0.88); // C(pi+)=0.88, C(pi-)=0.93
-int                       GetBeamHelicity (clas12reader c12, int runnum, int fdebug);
+int                       GetBeamHelicity (event_ptr p_event, int runnum, int fdebug);
 double                      GetBeamEnergy (clas12reader c12, int fdebug);
 
 // Oo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.
@@ -323,19 +323,14 @@ void SIDISc12rSkimmer(int  RunNumber=6420,
             runnum = c12.runconfig()->getRun();
             evnum  = c12.runconfig()->getEvent();
             if (fdebug>3) std::cout << "get beam helicity " << std::endl;
-//            beam_helicity = GetBeamHelicity(c12, runnum, fdebug );
-            beam_helicity = c12.event()->getHelicity();
-            
-            if (fdebug>3) std::cout << "check spin flip" << std::endl;
-            // we are working here on RGB data
-            bool helFlip = true;
-            if      (runnum>=11093 && runnum<=11283)    helFlip = false; // falls, 10.4 GeV period only
-            else if (runnum>=11323 && runnum<=11571)    helFlip = false; // winter
-            
-            if (helFlip) {
-                beam_helicity = -1 * beam_helicity;
-            }
-            if (fdebug>3) std::cout << "done GetBeamHelicity() " << std::endl;
+            GetBeamHelicity( c12.event() , runnum, fdebug );
+//            beam_helicity = c12.event()->getHelicity();
+//            // check spin flip
+//            // we are working here on RGB data
+//            bool helFlip = true;
+//            if      (runnum>=11093 && runnum<=11283)    helFlip = false; // falls, 10.4 GeV period only
+//            else if (runnum>=11323 && runnum<=11571)    helFlip = false; // winter
+//            if (helFlip) beam_helicity = -1 * beam_helicity;
             
             
             
@@ -1072,7 +1067,10 @@ double FindCutValue( std::string cutName ){
 
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-int GetBeamHelicity( clas12reader c12, int runnum, int fdebug ){
+int GetBeamHelicity( event_ptr p_event, int runnum, int fdebug ){
+    // deprecated as of Aug-11, 2021,
+    //    since
+    // however we keep it here for more data
     // get beam helicity (+1 along the beam and -1 opposite to it)
     // [Christopher Dilks <dilks@jlab.org>, email from Aug-5, 2021]
     // for more items
@@ -1090,7 +1088,7 @@ int GetBeamHelicity( clas12reader c12, int runnum, int fdebug ){
     //else if(RG=="RGF") helFlip = true
     if (fdebug>3) std::cout << "beam_helicity = c12.event()->getHelicity()" << std::endl;
     
-    beam_helicity = c12.event()->getHelicity();
+    beam_helicity = p_event->getHelicity();
     
     if (fdebug>3) std::cout << "check spin flip" << std::endl;
     // we are working here on RGB data
