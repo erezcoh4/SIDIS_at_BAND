c54ec42c0e01752c27189ea92335c2c9400afe96
erezcoh4 <cohen.erez7@gmail.com>
2022-01-20 09:36:35 +0200

    debug...

--- Read_PiAcceptance_GEMCimulations.C
+++ Read_PiAcceptance_GEMCimulations.C
@@ -249,7 +249,7 @@ void Read_PiAcceptance_GEMCimulations(TString fPiCharge = "pips",
                 
                 if (fdebug>2){
                     std::cout << "MC particle PDG code " << pid
-                    << std::setprecision(2)
+                    << std::setprecision(4)
                     << ", p: "<< P_mc_particle.P() << " GeV/c, "
                     << ", theta: "<< P_mc_particle.Theta()*r2d << " deg, "
                     << ", phi: "<< P_mc_particle.Phi()*r2d << " deg, "
