a5c88d17a224eeea862303bec3f7abf84f355d83
erezcoh4 <cohen.erez7@gmail.com>
2022-07-27 20:46:59 +0300

    Update MergeSIDISandBANDSkimmers.C

--- MergeSIDISandBANDSkimmers.C
+++ MergeSIDISandBANDSkimmers.C
@@ -850,7 +850,7 @@ void ComputeKinematics(){
     Q2      = -q->Mag2();
     omega   = q->E();
     
-    W2      = (target + *q).Mag2();
+    W2      = (*target + *q).Mag2();
     W       = sqrt(W2);
     
     W2      = (-Pn + *q).Mag2();
